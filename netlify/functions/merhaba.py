b579710ac45fb0d316f8068ced723783f4347593
hasanercan45 <hasanercan4@gmail.com>
2025-08-26 10:11:32 +0300

    Delete netlify directory

--- netlify/functions/merhaba.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def handler(event, context):
-    message = "Merhaba, dunya! Test fonksiyonu calisti."
-    return {
-        'statusCode': 200,
-        'headers': { 'Content-Type': 'application/json' },
-        'body': json.dumps({'message': message})
-    }
